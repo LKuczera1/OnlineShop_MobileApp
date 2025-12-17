4a836379cd684a155a7cb589b647abb8ec6fe81b
Łukasz Kuczera <lukaszkuczera1@gmail.com>
2025-12-17 17:34:51 +0100

    v0.1 - Small layout changes

---
+++ Services/IService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop_MobileApp.Services
+{
+    public class Service
+    {
+        protected readonly HttpClient _httpClient;
+
+        public Service(HttpClient client)
+        {
+            _httpClient = client;
+        }
+    }
+}
